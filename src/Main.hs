3a36e4f4d43b31bd2d50f0c90c3e41255afa9cb9
George Miller <geo.m.miller@gmail.com>
2017-07-28 10:11:01 -0600

    Works with eplicit app definition

--- src/Main.hs
+++ src/Main.hs
@@ -3,9 +3,22 @@ module Main where
 import qualified Astar
 import qualified Mapgen
 import Brick
+import Graphics.Vty
 
 ui :: Widget ()
 ui = str "Hello, world!"
+
+eventHandler :: s -> BrickEvent n e -> EventM n (Next s)
+eventHandler state e = continue state
+
+app :: App s e ()
+app = App
+  { appDraw = const [ui]
+  , appChooseCursor = showFirstCursor
+  , appStartEvent = return
+  , appAttrMap = const $ attrMap (white `on` black) []
+  , appHandleEvent = eventHandler
+  }
 
 main :: IO ()
 main = simpleMain ui
